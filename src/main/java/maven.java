70a9f5e8334c9f63ae6ae4f6bd578c2d6bafbb92
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-02-13 14:09:39 -0600

    Finished the exercise.

--- src/main/java/maven.java
+++ src/main/java/maven.java
@@ -1,5 +1,28 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 public class maven {
     public static void main(String[] args) {
         System.out.println("Enter something: ");
+        Scanner scanner = new Scanner(System.in);
+        String option = scanner.nextLine();
+        System.out.println("You entered: " + "\"" + option + "\"");
+        System.out.println("Is " + "\"" + option + "\"" + " a number? " + isNumeric(option));
+        System.out.println("Flipped Case: " + swapCase(option));
+        System.out.println("Reversed: " + reverse(option));
     }
+
+    public static boolean isNumeric(String option) {
+       return StringUtils.isNumeric(option);
+    }
+
+    public static String swapCase(String option) {
+        return StringUtils.swapCase(option);
+    }
+
+    public static String reverse(String option) {
+        return StringUtils.reverse(option);
+    }
+
 }
